8d8025966cfd551e60e036bcc18cf9b305753aba
jmhmjayamaha <jmhmjayamaha@gmail.com>
2017-10-13 12:47:00 +0530

    initial project structure

---
+++ src/lk/harshana/Main.java
@@ -0,0 +1,18 @@
+package lk.harshana;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Harshana's BrickBreake Game");
+		obj.setVisible(true);
+		obj.setResizable(false);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
